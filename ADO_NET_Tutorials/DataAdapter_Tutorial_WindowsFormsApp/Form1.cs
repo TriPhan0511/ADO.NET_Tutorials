9130a6ea589241b71310f6d4d0aad25c3c70c5b9
Tri Phan <phanphutri@gmail.com>
2022-05-08 10:30:14 +0700

    Updated

--- ADO_NET_Tutorials/DataAdapter_Tutorial_WindowsFormsApp/Form1.cs
+++ ADO_NET_Tutorials/DataAdapter_Tutorial_WindowsFormsApp/Form1.cs
@@ -19,33 +19,126 @@ namespace DataAdapter_Tutorial_WindowsFormsApp
             InitializeComponent();
         }
 
-        // 1. Fill the DataSet with records from a DataTable
+        // 3. 
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    // Create a DataTable instance named employeeTable
+        //    DataTable employeeTable = new DataTable();
+        //    DataColumn employeeIDColumn = new DataColumn("ID", typeof(Int32));
+        //    DataColumn employeeNameColumn = new DataColumn("Name", typeof(String));
+        //    DataColumn employeeCityColumn = new DataColumn("City", typeof(string));
+        //    employeeTable.Columns.Add(employeeIDColumn);
+        //    employeeTable.Columns.Add(employeeNameColumn);
+        //    employeeTable.Columns.Add(employeeCityColumn);
+        //    employeeTable.Rows.Add(1, "Alex", "Manchester");
+        //    employeeTable.Rows.Add(2, "Peter", "New York");
+        //    employeeTable.Rows.Add(3, "Hillary", "Melbourne");
+
+        //    // Create a DataTable instance named teacherTable
+        //    DataTable teacherTable = new DataTable();
+        //    DataColumn teacherIDColumn = new DataColumn("ID", typeof (String));
+        //    DataColumn teacherNameColumn = new DataColumn("Name", typeof (String));
+        //    DataColumn subjectColumn = new DataColumn("Subject", typeof (String));
+        //    teacherTable.Columns.Add(teacherIDColumn);
+        //    teacherTable.Columns.Add(teacherNameColumn);
+        //    teacherTable.Columns.Add(subjectColumn);
+        //    teacherTable.Rows.Add(4, "David Teacher", "London");
+
+        //    // Create a DataSet instance and add all DataTable instances to the DataSet instance
+        //    DataSet dataSet = new DataSet();
+        //    dataSet.Tables.Add(employeeTable);
+
+        //    dataGridView1.DataSource = dataSet;
+        //}
+        //// ------------------------------------------------------------------------------------------------
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            // Create a DataTable instance
-            DataTable dataTable = new DataTable();
+            // Use the MakeNamesTable method to create a new table
+            DataTable table = MakeNamesTable();
 
-            // Create three DataColumn instances
-            DataColumn idColumn = new DataColumn("ID", typeof(Int32));
-            DataColumn nameColumn = new DataColumn("Name", typeof(String));
-            DataColumn cityColumn = new DataColumn("City", typeof(String));
+            // Use NewRow method to create a DataRow
+            DataRow row = table.NewRow();
 
-            // Add three columns to the dataTable's collection of columns
-            dataTable.Columns.Add(idColumn);
-            dataTable.Columns.Add(nameColumn);
-            dataTable.Columns.Add(cityColumn);
+            //row["fname"] = "John";
+            //row["lname"] = "Smith";
+            //table.Rows.Add(row);
+            table.Rows.Add("David", "Beckham");
 
-            // Create a row using specifed values and add it to the dataTable's collection of rows
-            dataTable.Rows.Add(1, "Alex Ferguson", "Manchester");
-            dataTable.Rows.Add(2, "Peter Pan", "New York");
-            dataTable.Rows.Add(3, "Hillary Duff", "San Diego");
+            //DataRow row2 = table.NewRow();
+            //row2["fname"] = "Alex";
+            //row2["lname"] = "Ferguson";
+            //table.Rows.Add(row2);
 
-            // Set data for dataGridView's DataSource
-            dataGridView1.DataSource = dataTable;
+            //foreach (DataColumn column in table.Columns)
+            //{
+            //    Console.WriteLine(column.ColumnName);
+            //}
+
+            dataGridView1.DataSource = table;
         }
-        // ------------------------------------------------------------------------------------------------
 
-        // 2. Fill the DataSet with records from the SqlDataAdapter
+        private DataTable MakeNamesTable()
+        {
+            // Create a new DataTable titled "Names"
+            DataTable namesTable = new DataTable("Names");
+
+            // Add three DataColumn objects to the table
+            DataColumn idColumn = new DataColumn();
+            idColumn.DataType = Type.GetType("System.Int32");
+            idColumn.ColumnName = "ID";
+            idColumn.AutoIncrement = true;
+            namesTable.Columns.Add(idColumn);
+
+            DataColumn fnameColumn = new DataColumn();
+            fnameColumn.DataType = Type.GetType("System.String");
+            fnameColumn.ColumnName = "fName";
+            fnameColumn.DefaultValue = "Fname";
+            namesTable.Columns.Add(fnameColumn);
+
+            DataColumn lnameColumn = new DataColumn();
+            lnameColumn.DataType = Type.GetType("System.String");
+            lnameColumn.ColumnName = "lname";
+            lnameColumn.DefaultValue = "Lname";
+            namesTable.Columns.Add(lnameColumn);
+
+            // Create an array for DataColumn objects
+            // and set PrimaryKey to that array
+            DataColumn[] keys = new DataColumn[1];
+            keys[0] = idColumn;
+            namesTable.PrimaryKey = keys;
+
+            // Return the new DataTable
+            return namesTable;
+        }
+
+        //// 2. Fill the DataSet with records from a DataTable
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    // Create a DataTable instance
+        //    DataTable dataTable = new DataTable();
+
+        //    // Create three DataColumn instances
+        //    DataColumn idColumn = new DataColumn("ID", typeof(Int32));
+        //    DataColumn nameColumn = new DataColumn("Name", typeof(String));
+        //    DataColumn cityColumn = new DataColumn("City", typeof(String));
+
+        //    // Add three columns to the dataTable's collection of columns
+        //    dataTable.Columns.Add(idColumn);
+        //    dataTable.Columns.Add(nameColumn);
+        //    dataTable.Columns.Add(cityColumn);
+
+        //    // Create a row using specifed values and add it to the dataTable's collection of rows
+        //    dataTable.Rows.Add(1, "Alex Ferguson", "Manchester");
+        //    dataTable.Rows.Add(2, "Peter Pan", "New York");
+        //    dataTable.Rows.Add(3, "Hillary Duff", "San Diego");
+
+        //    // Set data for dataGridView's DataSource
+        //    dataGridView1.DataSource = dataTable;
+        //}
+        //// ------------------------------------------------------------------------------------------------
+
+        // 1. Fill the DataSet with records from the SqlDataAdapter
         //private void Form1_Load(object sender, EventArgs e)
         //{
         //    // Declare a connection string
